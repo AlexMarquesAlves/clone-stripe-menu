742c40674fe9ce08dc4182c349083182319f2bba
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-15 02:11:49 -0300

    Wrapping Components

--- src/components/Dropdown/Root.js
+++ src/components/Dropdown/Root.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { motion } from "framer-motion";
+
+import { Context } from "./Provider";
+import { DropdownSection } from "./Section";
 
 export function DropdownRoot() {
-  return <div />;
+  const { options } = useContext(Context);
+  return (
+    <div className="dropdown-root">
+      <div>
+        <div className="dropdown-container">
+          {options.map((item) => (
+            <DropdownSection key={item.id} option={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
