8b5e4ccba7a2d0c41908765c93364bb66ee29584
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-15 22:33:03 -0300

    Putting backgroundHeight prop on Nabar index

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -11,13 +11,25 @@ function NavBar() {
         <Container>
           <ul>
             <li>
-              <DropdownOption name="Produtos" content={Products} />
+              <DropdownOption
+                name="Produtos"
+                content={Products}
+                backgroundHeight={286}
+              />
             </li>
             <li>
-              <DropdownOption name="Desenvolvedores" content={Developers} />
+              <DropdownOption
+                name="Desenvolvedores"
+                content={Developers}
+                backgroundHeight={286}
+              />
             </li>
             <li>
-              <DropdownOption name="Empresa" content={Company} />
+              <DropdownOption
+                name="Empresa"
+                content={Company}
+                backgroundHeight={286}
+              />
             </li>
           </ul>
         </Container>
